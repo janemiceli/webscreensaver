afb7afeea463cc1994b17c81b512abb622c8b6a6
Jane Miceli <jane@janemiceli.com>
2014-07-21 02:22:13 -0600

    from code.google.com

---
+++ PreferencesForm.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using Microsoft.Win32;
+using System.Text;
+using System.Windows.Forms;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    public partial class PreferencesForm : Form
+    {
+        public PreferencesForm()
+        {
+            InitializeComponent();
+        }
+
+        private void PreferencesForm_Load(object sender, EventArgs e)
+        {
+            RegistryKey reg = Registry.CurrentUser.CreateSubKey(Program.KEY);
+            textBox1.Text = (string)reg.GetValue("Url", "http://www.polidea.pl");
+            reg.Close();
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            if (DialogResult == DialogResult.OK)
+            {
+                RegistryKey reg = Registry.CurrentUser.CreateSubKey(Program.KEY);
+                reg.SetValue("Url", textBox1.Text);
+                reg.Close();
+            }
+
+            base.OnClosed(e);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(linkLabel1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using pl.polidea.lab.Web_Page_Screensaver;
+
+namespace pl.polidea.lab.Web_Page_Screensaver
+{
+    static class Program
+    {
+        public static readonly string KEY = "Software\\Web-Page-Screensaver";  
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            if (args.Length > 0 && args[0].ToLower().Contains("/p"))
+                return;
+
+            if(args.Length > 0 && args[0].ToLower().Contains("/c"))
+                Application.Run(new PreferencesForm());
+            else
+                Application.Run(new ScreensaverForm());
+        }
+    }
+}
